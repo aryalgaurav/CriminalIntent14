0b399a4fb62eef62b76935dc44bedc3cc71a6986
aryalg1@nku.edu <aryalg1@nku.edu>
2016-09-01 13:11:18 -0400

    Initialize Chapter 14.

---
+++ app/src/main/java/android/nku/edu/criminalintent/CrimeLab.java
@@ -0,0 +1,49 @@
+package android.nku.edu.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+/**
+ * Created by Gaurav on 8/16/2016.
+ */
+public class CrimeLab {
+
+    private static CrimeLab sCrimeLab;
+    private List<Crime> mCrimes;
+
+    private CrimeLab(Context context) {
+        mCrimes = new ArrayList<>();
+    }
+
+    public static CrimeLab get(Context context) {
+        if (sCrimeLab == null) {
+            sCrimeLab = new CrimeLab(context);
+        }
+        return sCrimeLab;
+    }
+
+    public void addCrime(Crime crime) {
+        mCrimes.add(crime);
+    }
+
+    public void deleteCrime (UUID crimeId) {
+        Crime crime = getCrime(crimeId);
+        mCrimes.remove(crime);
+    }
+
+    public List<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime crime : mCrimes) {
+            if (crime.getId().equals(id)) {
+                return crime;
+            }
+        }
+        return null;
+    }
+}
